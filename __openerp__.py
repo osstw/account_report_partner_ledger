f6716172c760f2a018b3141c5681f10bc7f002eb
wangting <39181819@qq.com>
2016-02-20 17:07:25 +0800

    init commit

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+{
+    'name': "account_report_partner_ledger",
+    'version': '1.0',
+    'depends': ["account"],
+    'author': "糖葫芦(39181819@qq.com)",
+    'category': '',
+    'data': ["views/report_partnerledger.xml",
+             "views/report_partnerledgerother.xml",
+             "wizard/account_report_partner_ledger_view.xml",
+             "account_report.xml"],
+    'demo': [],
+    'description': """
+    自定义的报表,加入invoice origin字段。
+
+    """,
+}
